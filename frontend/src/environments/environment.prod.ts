cff309070e33e25a314bbe1f5c04b3d3d61c6b9f
NamAnh905 <namanhdang0905@gmail.com>
2025-10-25 22:09:40 +0700

    Quick fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://movie-be-hecl.onrender.com' // cập nhật sau khi deploy BE
+  baseUrl: 'https://movie-be-hecl.onrender.com', // cập nhật sau khi deploy BE
+  api: {
+    auth: '/api/auth',
+    users: '/api/users',
+    movies: '/api/movies',
+    genres: '/api/genres',
+    cinemas: '/api/cinemas',
+    showtimes: '/api/showtimes',
+    files: '/api/files',
+    uploads: '/uploads'
+  }
 };
